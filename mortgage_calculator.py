d0dc175957ec7c26d221ee1c7b55ebc22cf3e5d7
Sreekaanth Ganesan <sreekaanthg91@gmail.com>
2022-09-21 07:40:11 -0400

    Fixed some cosmetic issues

--- mortgage_calculator.py
+++ mortgage_calculator.py
@@ -7,7 +7,7 @@ def calc_monthly_payment(loan_amount,interest_rate,loan_duration_in_months):
     # M = loan_amount[interest_rate(1+interest_rate)loan_duration_in_months] / [(1+interest_rate)loan_duration_in_months-1]
     mortgagePayment = loan_amount * (interest_rate * (1 + interest_rate)
                                 ** loan_duration_in_months) / ((1 + interest_rate) ** loan_duration_in_months - 1)
-    print(mortgagePayment)
+    return ("The monthly mortgage payment is \n (%.2f)"%mortgagePayment)
     #M = L[i(1+i)n] / [(1+i)n-1]
     #   * M = Monthly Payment (what were trying to find out)
     #   * L = Loan Amount (loanAmount)
@@ -16,14 +16,15 @@ def calc_monthly_payment(loan_amount,interest_rate,loan_duration_in_months):
 
 if __name__=='__main__':
     
-    loan_amount = input("What is the loan amount would you like to borrow?")
-    interest_rate=input("What is the interest rate on your loan?")
-    loan_duration=input("What is the loan duration in years to repay the loan")
+    loan_amount = input("What is the loan amount would you like to borrow?: \t")
+    interest_rate=input("What is the interest rate on your loan?: \t")
+    loan_duration=input("What is the loan duration in years to repay the loan: \t")
 
     loan_duration_in_months=float(loan_duration)*12
     # print(loan_duration_in_months)
     interest_rate=float(interest_rate) /100/12
     loan_amount=float(loan_amount)
 
-    calc_monthly_payment(loan_amount,interest_rate,loan_duration_in_months)
+    monthly_payment = calc_monthly_payment(loan_amount,interest_rate,loan_duration_in_months)
+    print (monthly_payment)
 
